37e54a79de10518d52768a2d977d2c430d34866f
Mikhail Krushelnitskiy <bluextchannel@gmail.com>
2023-04-28 15:42:17 +0300

    Created domain entities

---
+++ src/Theses.Domain/Entities/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Theses.Domain.Entities;
+
+public record Person(string FirstName, string MiddleName, string LastName, string Workplace)
+{
+    public Person(string FirstName, string LastName, string Workplace)
+        : this(FirstName, string.Empty, LastName, Workplace) { }
+
+    public long Id { get; }
+    public string FirstName { get; set; } = FirstName;
+    public string? MiddleName { get; set; } = MiddleName;
+    public string LastName { get; set; } = LastName;
+    public string Workplace { get; set; } = Workplace;
+}
---
+++ src/Theses.Domain/Entities/Thesis.cs
@@ -0,0 +1,13 @@
+﻿namespace Theses.Domain.Entities;
+
+public record Thesis(Person MainAuthor, string ContactEmail, ICollection<Person> OtherAuthors, string Topic, string Content)
+{
+    public long Id { get; }
+    public Person MainAuthor { get; set; } = MainAuthor;
+    public string ContactEmail { get; set; } = ContactEmail;
+    public ICollection<Person>? OtherAuthors { get; set; } = OtherAuthors;
+    public string Topic { get; set; } = Topic;
+    public string Content { get; set; } = Content;
+    public DateTime Created { get; } = DateTime.UtcNow;
+    public DateTime Updated { get; set; } = DateTime.UtcNow;
+}
