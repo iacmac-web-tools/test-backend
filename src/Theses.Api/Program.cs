98fafb9deb7f5fdd7e449130175b96706d27402e
Mikhail Krushelnitskiy <bluextchannel@gmail.com>
2023-04-28 19:29:32 +0300

    Created api project

---
+++ src/Theses.Api/Program.cs
@@ -0,0 +1,24 @@
+using Theses.Application;
+using Theses.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => options.SupportNonNullableReferenceTypes());
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
