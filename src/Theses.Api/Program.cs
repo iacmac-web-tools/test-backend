943f7f1e38c8fdeebb141dd7ddd17f194579c09e
Mikhail Krushelnitskiy <bluextchannel@gmail.com>
2023-04-28 23:56:15 +0300

    Added integration tests

--- src/Theses.Api/Program.cs
+++ src/Theses.Api/Program.cs
@@ -22,3 +22,5 @@ if (app.Environment.IsDevelopment())
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
---
+++ tests/Theses.IntegrationTests/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Theses.Application;
+using Theses.Infrastructure;
+
+namespace Theses.IntegrationTests;
+
+public class Startup
+{
+    public void ConfigureHost(IHostBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureHostConfiguration(builder => builder.AddJsonFile("appsettings.json", false));
+
+        hostBuilder.ConfigureServices((context, services) => {
+            services.AddApplicationServices();
+            services.AddInfrastructureServices(context.Configuration);
+        });
+    }
+}
